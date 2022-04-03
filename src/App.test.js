d5101f4147a1ba35ddff2067ec688fccfa46817e
Julie Nisbet <jmnisbet@gmail.com>
2022-04-03 16:11:23 -0700

    fixing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders different players', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const player1 = screen.getByText(/Player 1/i);
+  const player2 = screen.getByText(/Player 2/i);
+  const player3 = screen.getByText(/Player 3/i);
+  expect(player1).toBeInTheDocument();
+  expect(player2).toBeInTheDocument();
+  expect(player3).toBeInTheDocument();
 });
