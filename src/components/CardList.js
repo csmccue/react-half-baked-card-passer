c925132940c1db07e4c842759f8665435b6cc04e
Connor McCue <connor.mccue@comcast.net>
2022-10-03 15:57:42 -0700

    removed contest from cardlist.js, not needed

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import Card from './Card';
-import { useContext } from 'react';
-import { GameContext } from '../context/GameContext';
 
 
 export default function CardList({ cards, player }) {
-  const { selectedCard, setSelectedCard, setFrom } = useContext(GameContext);
   return (
     <div className="card-list">
       {cards.map((card) => (
         <Card
           key={card.suit + card.value}
-          setSelectedCard={setSelectedCard}
-          selectedCard={selectedCard}
           player={player}
-          setFrom={setFrom}
           card={card}
         />
       ))}
