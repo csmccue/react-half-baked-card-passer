5b3e9b50e294268df979e3035aa67e44b3f08d2e
Connor McCue <connor.mccue@comcast.net>
2022-10-03 15:48:28 -0700

    removed excess prop drilling from app.js

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ function App() {
     playerThreeHand,
     setPlayerThreeHand,
     from,
-    setFrom,
-    to,
-    setTo
+    to
   } = useContext(GameContext);
 
 
@@ -55,37 +53,19 @@ function App() {
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
         <Player
-          to={to}
           player={1}
           hand={playerOneHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
         />
         <Player
-          to={to}
           player={2}
           hand={playerTwoHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
         />
         <Player
-          to={to}
           player={3}
           hand={playerThreeHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
         />
         <CardList
           cards={deck}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-          setFrom={setFrom}
           player={'deck'}
         />
       </section>
@@ -93,11 +73,6 @@ function App() {
         {selectedCard && (
           <ExecutePassButton
             passCard={passCard}
-            setFrom={setFrom}
-            from={from}
-            to={to}
-            selectedCard={selectedCard}
-            setSelectedCard={setSelectedCard}
           />
         )}
       </section>
