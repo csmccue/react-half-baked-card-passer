675ffe2fc7d0615b8e897b39542be33a34c00fd8
Connor McCue <connor.mccue@comcast.net>
2022-10-03 12:00:59 -0700

    GameContext.js updated to reflect state

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,10 +1,18 @@
 import { createContext, useState } from 'react';
+import initialCards from './cards-data';
 
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-  const [user, setUser] = useState('');
-  return <GameContext.Provider value = {{ user, setUser }}>{children}</GameContext.Provider>;
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+  return <GameContext.Provider value = {{ deck, setDeck }}>{children}</GameContext.Provider>;
 };
 
 export { GameProvider, GameContext };
